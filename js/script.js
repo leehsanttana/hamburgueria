cbfb8e9466098689dfbad8690347dc9581358e39
leehsanttana <leonamsanttana@gmail.com>
2021-03-30 16:27:23 -0300

    finalizando o projeto. foram acrescentados todos os conteudos iniciais e feito a responsividade que ainda precisa ser testada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 const tab = document.querySelectorAll('[data-tab="tab"] li');
 const content = document.querySelectorAll('[data-tab="content"] div');
+const buttonBar = document.querySelector('.btn-menu');
+const nav = document.querySelector('.nav');
+
+function showMenu() {
+  nav.classList.toggle('show-menu');
+  buttonBar.classList.toggle('active');
+}
+
+function activeShowMenu() {
+  buttonBar.addEventListener('click', showMenu);
+}
+
+activeShowMenu();
+
+function scrollInternalLink() {
+  const links = document.querySelectorAll('.nav a[href^="#"]');
+
+  function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+  }
+
+  function scrollToPosition(to) {
+    window.scroll({
+      top: to,
+      behavior: 'smooth',
+    });
+  }
+
+  function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.currentTarget) - 60;
+    scrollToPosition(to);
+    nav.classList.remove('show-menu');
+  }
+
+  links.forEach((links, i) => {
+    links.addEventListener('click', scrollToIdOnClick);
+  });
+}
+
+scrollInternalLink();
 
 function menuTabs() {
   tab[0].classList.add('active');
